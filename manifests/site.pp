9c76f653661a44c007324b5f16287a7f924d5887
root <root@jessie.raw>
2016-07-15 10:14:19 +0000

    Modified site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,31 +1,20 @@
-#node 'agent3'{ 
-#	wget::fetch {'fetch':
- #           source      => 'https://www.gerritcodereview.com/download/gerrit-2.8.1.war',
-  #          destination => '/opt/gerrit/',
-  #      }
-#include gerrit
-#include jenkins
-# }
+node 'agent1.example.com'{ 
+include gerrit
 
-node 'agent3.example.com'{
-include jenkins
-include jenkins::master
+class { 'jenkins::slave':
+   		 masterurl => 'http://agent3.example.com:8080',
+   		 ui_user => 'adminuser',
+   		 ui_pass => 'adminpass', 
+		}
 }
 
-node 'agent4.example.com' {
-  class { 'jenkins::slave':
-    masterurl => 'http://agent3.example.com:8080',
-    ui_user => 'adminuser',
-    ui_pass => 'adminpass',
-  }
+node 'agent2.example.com'{
+	include jenkins
+	include jenkins::master
 }
-#node 'agent2.example.com'{
-#include jenkins
-#}
 
 node default{
-user {'test':
-ensure => 'present',
-}
-
+	user {'test':
+		ensure => 'present',
+	}
 }
